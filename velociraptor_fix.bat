0b3a892f05a0eeb331332706e060e701ebb667e0
ibyf0r3ns1cs <50079464+ibyf0r3ns1cs@users.noreply.github.com>
2026-02-01 10:57:09 +0200

    Update velociraptor_fix.bat

--- velociraptor_fix.bat
+++ velociraptor_fix.bat
@@ -1,5 +1,18 @@
 @echo off
-set LOGFILE=C:\ProgramData\velociraptor_fix_log.txt
+setlocal EnableDelayedExpansion
+
+REM Build timestamp YYYYMMDD_HHMMSS
+set YYYY=%DATE:~10,4%
+set MM=%DATE:~4,2%
+set DD=%DATE:~7,2%
+set HH=%TIME:~0,2%
+set MI=%TIME:~3,2%
+set SS=%TIME:~6,2%
+
+REM Remove leading space from hour if exists
+if "%HH:~0,1%"==" " set HH=0%HH:~1,1%
+
+set LOGFILE=C:\ProgramData\velociraptor_fix_%YYYY%%MM%%DD%_%HH%%MI%%SS%.log
 set VELO=C:\Program Files\Velociraptor\Velociraptor.exe
 set CONF=C:\Program Files\Velociraptor\client.config.yaml
 
@@ -27,17 +40,17 @@ start "" /b "%VELO%" --config "%CONF%" service run -v --debug >> "%LOGFILE%" 2>&
 REM Give process time to initialize
 timeout /t 5 >nul
 
-REM Capture PID of the debug instance
+REM Capture PID of the debug instance (best effort)
 for /f "tokens=2 delims== " %%P in (
   'wmic process where "name='Velociraptor.exe'" get ProcessId /value ^| find "="'
 ) do set PID=%%P
 
-echo Velociraptor debug PID=%PID% >> "%LOGFILE%"
+echo Velociraptor debug PID=!PID! >> "%LOGFILE%"
 
 timeout /t 120 /nobreak >nul
 
-echo Stopping manual Velociraptor process PID %PID% ...
-taskkill /f /pid %PID% >> "%LOGFILE%" 2>&1
+echo Stopping manual Velociraptor process PID !PID! ...
+taskkill /f /pid !PID! >> "%LOGFILE%" 2>&1
 
 echo Waiting 5 seconds before restarting service...
 timeout /t 5 >nul
@@ -49,3 +62,5 @@ echo [%DATE% %TIME%] Velociraptor fix completed >> "%LOGFILE%"
 echo. >> "%LOGFILE%"
 
 echo Done. Log written to %LOGFILE%
+
+endlocal
